84129c4bd280af186836347e904b7e6ef728dcba
Philip-soon <76766596+Philip-soon@users.noreply.github.com>
2025-04-25 10:01:17 +0900

    action test 1

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-/// 테스트 코드
+/// 테스트 코드 1
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
