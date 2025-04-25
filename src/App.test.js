db083ed83460c4c85da5ec519bacae00ef2cf221
Philip-soon <76766596+Philip-soon@users.noreply.github.com>
2025-04-25 09:44:41 +0900

    docker test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+/// 테스트 코드
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
